ae88c83cdfe062ef63b058af60e3f719fb9665d1
Simone Amorim <simoneas02@hotmail.com>
2016-04-19 00:40:43 -0300

    Adicionado parâmetos dos valores para form o cadastro

--- cadastrar.php
+++ cadastrar.php
@@ -6,14 +6,35 @@
     $con->connect(); // abre conexao com o banco
 
 
-    //Pega os valores do formulário de cadastro através do atributo NAME
+    //Pega os valores do formulário de cadastro - Dados do Clube
     $nome = $_POST['nome'];  
-    
+    $nomeconjugue = $_POST['nomeconjugue'];  
+    $naturalidade = $_POST['naturalidade'];  
+    $estado = $_POST['estado'];  
+    $funcao = $_POST['funcao'];  
+    $datanascimento = $_POST['datanascimento'];  
+    $email = $_POST['email'];
+    $endereco = $_POST['endereco'];
+    $telefone = $_POST['telefone'];
+
+    //Pega os valores do formulário de cadastro - Dados Pessoais
+    $matricula = $_POST['matricula'];
+    $nomeclube = $_POST['nomeclube'];
+    $regiao = $_POST['regiao'];
+    $comissao = $_POST['comissao'];
+    $ingressolions = $_POST['ingressolions'];
+    $melvinjones = $_POST['melvinjones'];
+    $buttonenviar = $_POST['buttonenviar'];
+
     // instancia classe com as operaçoes crud, passando o nome da tabela como parametro
     $crud = new crud('tb_membros');  
 
     // utiliza a funçao INSERIR da classe crud
-    $crud->inserir("nome,descricao", "'$nome','$descricao'"); 
+    $crud->inserir("nome, nomeconjugue, naturalidade, estado, funcao, datanascimento, email, 
+                    endereco, telefone, matricula, nomeclube, regiao, comissao, ingressolions, 
+                    melvinjones, buttonenviar", "'$nome', '$nomeconjugue', '$naturalidade', '$estado', 
+                    '$funcao', '$datanascimento', '$email', '$endereco', '$telefone', '$matricula', 
+                    '$nomeclube', '$regiao', '$comissao', '$ingressolions', '$melvinjones', '$buttonenviar'"); 
 
     header("Location: index.php"); // redireciona para a listagem
 ?>
