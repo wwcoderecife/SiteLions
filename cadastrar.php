bde0664b73a0b96736475c1ff4d68a3e5be2efe8
WWcode Recife <karina@womenwhocode.com>
2016-05-18 12:56:08 -0300

    Update cadastrar.php
    // Condição para aceitar campo vazio no tipo date, fiz comentário , mas não funcionou no cadastro

--- cadastrar.php
+++ cadastrar.php
@@ -30,14 +30,16 @@
     $ingressolions = $_POST['ingressolions'];
     $melvinjones = $_POST['melvinjones'];
 
-    
-if(is_null($datanascimento)){
+// Condição para aceitar campo vazio no tipo date.   
+/*if(is_null($datanascimento)){
     $datanascimento = 'NULL';
 }
+*/
 
-if(is_null($ingressolions)){
+/*if(is_null($ingressolions)){
    $ingressolions = 'NULL';
 }
+*/
 
     // instancia classe com as operaçoes crud, passando o nome da tabela como parametro
     $crud = new crud('tb_membros');  
