a49679c6ef3792ac80c7e292bfae1ea1f6a69943
WWcode Recife <karina@womenwhocode.com>
2016-05-18 00:43:04 -0300

    Update relatorios.php

--- relatorios.php
+++ relatorios.php
@@ -676,7 +676,7 @@
     </div>
 
     <!-- Relatório Modal 10 / Suplentes -->
-  <!--  <div class="portfolio-modal modal fade" id="portfolioModal10" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="portfolio-modal modal fade" id="portfolioModal10" tabindex="-1" role="dialog" aria-hidden="true">
         <div class="modal-content">
             <div class="close-modal" data-dismiss="modal">
                 <div class="lr">
@@ -687,9 +687,9 @@
             <div class="container">
                 <div class="row">
                     <div class="col-lg-8 col-lg-offset-2">
-                        <div class="modal-body">-->
+                        <div class="modal-body">
                             <!-- Relatório Details Go Here -->
-                         <!--   <h2>Suplentes</h2>
+                           <h2>Suplentes</h2>
                             <button type='button' onclick='print_specific_div_content(\"print\",\"Membros\")' class='btn btn-default btn-sm' style='float: right'>
                               <span class='glyphicon glyphicon-print'></span> Imprimir
                             </button>
@@ -717,7 +717,7 @@
                 </div>
             </div>
         </div>
-    </div>-->
+    </div>
 
     <!-- Relatório Modal 11 / Folha de Votação / Delegados Natos -->
     <div class="portfolio-modal modal fade" id="portfolioModal11" tabindex="-1" role="dialog" aria-hidden="true">
