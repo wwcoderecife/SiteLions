bfd92dbf3d1d0f0700ab2e6516ef692e4ca60d2a
WWcode Recife <karina@womenwhocode.com>
2016-05-18 23:40:07 -0300

    Update relatorios.php

--- relatorios.php
+++ relatorios.php
@@ -52,18 +52,18 @@
 					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
 						<span class="fa fa-bars color-white"></span>
 					</button>
-					<h1><a class="navbar-brand" href="index.html" data-0="line-height:90px;" data-300="line-height:50px;">Lions Clube International - DMLA
+					<h1><a class="navbar-brand" href="index.html" data-0="line-height:90px;" data-300="line-height:50px;">Área Administrativa - Lions
 					</a></h1>
 				</div>
 				<div class="navbar-collapse collapse">
 					<ul class="nav navbar-nav" data-0="margin-top:20px;" data-300="margin-top:5px;">
-						<li class="active"><a href="#index">Home</a></li>
+						
 						<!--<li><a href="#section-about">Inscriçāo</a></li>-->
 						<!--<li><a href="#services">A Convençāo</a></li>-->
 						<!--<li><a href="#team">Comissāo Organizadora</a></li>-->
 						<!--<li><a href="#section-contact">Contato</a></li>-->
 						<!--<li><a href="#restrito">Área administrativa</a></li>-->	
-        				        <li><a href="#relatorio">Relatórios</a></li>
+        				        <li><a href="#relatorio">Sair</a></li>
 					</ul>
 				</div><!--/.navbar-collapse -->
 			</div>
